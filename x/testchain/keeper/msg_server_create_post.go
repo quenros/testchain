e719313df1678c320fba47f6ddeb845ed7237c24
Wei YAng <weiyang35@hotmail.com>
2024-08-09 18:18:38 +0800

    Create post

--- x/testchain/keeper/msg_server_create_post.go
+++ x/testchain/keeper/msg_server_create_post.go
@@ -9,10 +9,17 @@ import (
 )
 
 func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
-	return &types.MsgCreatePostResponse{}, nil
+    ctx := sdk.UnwrapSDKContext(goCtx)
+    var post = types.Post{
+        Creator: msg.Creator,
+        Title:   msg.Title,
+        Body:    msg.Body,
+    }
+    id := k.AppendPost(
+        ctx,
+        post,
+    )
+    return &types.MsgCreatePostResponse{
+        Id: id,
+    }, nil
 }
