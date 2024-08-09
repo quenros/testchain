2f4c52c684c17b9dbab2cb2530f2fb5d9714b425
Wei YAng <weiyang35@hotmail.com>
2024-08-09 18:25:45 +0800

    delete post

--- x/testchain/types/tx.pb.go
+++ x/testchain/types/tx.pb.go
@@ -332,6 +332,94 @@ func (m *MsgUpdatePostResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_MsgUpdatePostResponse proto.InternalMessageInfo
 
+type MsgDeletePost struct {
+	Creator string `protobuf:"bytes,1,opt,name=creator,proto3" json:"creator,omitempty"`
+	Id      uint64 `protobuf:"varint,2,opt,name=id,proto3" json:"id,omitempty"`
+}
+
+func (m *MsgDeletePost) Reset()         { *m = MsgDeletePost{} }
+func (m *MsgDeletePost) String() string { return proto.CompactTextString(m) }
+func (*MsgDeletePost) ProtoMessage()    {}
+func (*MsgDeletePost) Descriptor() ([]byte, []int) {
+	return fileDescriptor_c21a4640413a6f4a, []int{6}
+}
+func (m *MsgDeletePost) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgDeletePost) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgDeletePost.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgDeletePost) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgDeletePost.Merge(m, src)
+}
+func (m *MsgDeletePost) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgDeletePost) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgDeletePost.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgDeletePost proto.InternalMessageInfo
+
+func (m *MsgDeletePost) GetCreator() string {
+	if m != nil {
+		return m.Creator
+	}
+	return ""
+}
+
+func (m *MsgDeletePost) GetId() uint64 {
+	if m != nil {
+		return m.Id
+	}
+	return 0
+}
+
+type MsgDeletePostResponse struct {
+}
+
+func (m *MsgDeletePostResponse) Reset()         { *m = MsgDeletePostResponse{} }
+func (m *MsgDeletePostResponse) String() string { return proto.CompactTextString(m) }
+func (*MsgDeletePostResponse) ProtoMessage()    {}
+func (*MsgDeletePostResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_c21a4640413a6f4a, []int{7}
+}
+func (m *MsgDeletePostResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgDeletePostResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgDeletePostResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgDeletePostResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgDeletePostResponse.Merge(m, src)
+}
+func (m *MsgDeletePostResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgDeletePostResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgDeletePostResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgDeletePostResponse proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*MsgUpdateParams)(nil), "testchain.testchain.MsgUpdateParams")
 	proto.RegisterType((*MsgUpdateParamsResponse)(nil), "testchain.testchain.MsgUpdateParamsResponse")
@@ -339,12 +427,14 @@ func init() {
 	proto.RegisterType((*MsgCreatePostResponse)(nil), "testchain.testchain.MsgCreatePostResponse")
 	proto.RegisterType((*MsgUpdatePost)(nil), "testchain.testchain.MsgUpdatePost")
 	proto.RegisterType((*MsgUpdatePostResponse)(nil), "testchain.testchain.MsgUpdatePostResponse")
+	proto.RegisterType((*MsgDeletePost)(nil), "testchain.testchain.MsgDeletePost")
+	proto.RegisterType((*MsgDeletePostResponse)(nil), "testchain.testchain.MsgDeletePostResponse")
 }
 
 func init() { proto.RegisterFile("testchain/testchain/tx.proto", fileDescriptor_c21a4640413a6f4a) }
 
 var fileDescriptor_c21a4640413a6f4a = []byte{
-	// 458 bytes of a gzipped FileDescriptorProto
+	// 491 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x29, 0x49, 0x2d, 0x2e,
 	0x49, 0xce, 0x48, 0xcc, 0xcc, 0xd3, 0x47, 0x62, 0x55, 0xe8, 0x15, 0x14, 0xe5, 0x97, 0xe4, 0x0b,
 	0x09, 0xc3, 0xc5, 0xf4, 0xe0, 0x2c, 0x29, 0xc1, 0xc4, 0xdc, 0xcc, 0xbc, 0x7c, 0x7d, 0x30, 0x09,
@@ -366,14 +456,16 @@ var fileDescriptor_c21a4640413a6f4a = []byte{
 	0x2b, 0x1e, 0x90, 0x4b, 0x61, 0xfa, 0x94, 0xd4, 0xb9, 0x44, 0x51, 0xac, 0x80, 0xd9, 0x2d, 0xc4,
 	0xc7, 0xc5, 0x94, 0x99, 0x02, 0xb6, 0x85, 0x25, 0x88, 0x29, 0x33, 0x45, 0xa9, 0x10, 0xec, 0x16,
 	0xa8, 0x33, 0xa9, 0xe4, 0x16, 0xa8, 0x25, 0x2c, 0x30, 0x4b, 0xd0, 0xdc, 0x26, 0x0e, 0x76, 0x1b,
-	0xc2, 0x4a, 0x98, 0xdb, 0x8c, 0xd6, 0x30, 0x71, 0x31, 0xfb, 0x16, 0xa7, 0x0b, 0x25, 0x71, 0xf1,
-	0xa0, 0x44, 0xbe, 0x0a, 0xd6, 0x48, 0x43, 0x0b, 0x5d, 0x29, 0x1d, 0x62, 0x54, 0xc1, 0xc3, 0x21,
-	0x86, 0x8b, 0x0b, 0x29, 0x02, 0x94, 0x70, 0xe9, 0x45, 0xa8, 0x91, 0xd2, 0x22, 0xac, 0x06, 0xd9,
-	0x74, 0xa4, 0x20, 0x55, 0x22, 0xe0, 0x32, 0xbc, 0xa6, 0x63, 0x86, 0x93, 0x14, 0x6b, 0x03, 0x28,
-	0x8d, 0x3a, 0x99, 0x9e, 0x78, 0x24, 0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c,
-	0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43, 0x94, 0x34, 0xf6,
-	0x24, 0x5a, 0x52, 0x59, 0x90, 0x5a, 0x9c, 0xc4, 0x06, 0xce, 0x65, 0xc6, 0x80, 0x00, 0x00, 0x00,
-	0xff, 0xff, 0x3f, 0x9a, 0xee, 0x04, 0x19, 0x04, 0x00, 0x00,
+	0xc2, 0x4a, 0x78, 0xb8, 0xb8, 0x83, 0xdd, 0xe2, 0x92, 0x9a, 0x93, 0x4a, 0xd0, 0x2d, 0x10, 0x1b,
+	0x98, 0xf0, 0xda, 0x80, 0x30, 0x08, 0x66, 0x83, 0x51, 0x17, 0x33, 0x17, 0xb3, 0x6f, 0x71, 0xba,
+	0x50, 0x12, 0x17, 0x0f, 0x4a, 0xf2, 0x52, 0xc1, 0x9a, 0x2c, 0xd0, 0xe2, 0x4f, 0x4a, 0x87, 0x18,
+	0x55, 0xf0, 0x90, 0x8e, 0xe1, 0xe2, 0x42, 0x8a, 0x62, 0x25, 0x5c, 0x7a, 0x11, 0x6a, 0xa4, 0xb4,
+	0x08, 0xab, 0x41, 0x36, 0x1d, 0x29, 0xd2, 0x94, 0x08, 0xb8, 0x0c, 0xaf, 0xe9, 0x98, 0x31, 0x01,
+	0x32, 0x1d, 0x29, 0x1a, 0x70, 0x9a, 0x8e, 0x50, 0x83, 0xdb, 0x74, 0xcc, 0x58, 0x90, 0x62, 0x6d,
+	0x00, 0xe5, 0x31, 0x27, 0xd3, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c, 0x92, 0x63, 0x7c, 0xf0, 0x48,
+	0x8e, 0x71, 0xc2, 0x63, 0x39, 0x86, 0x0b, 0x8f, 0xe5, 0x18, 0x6e, 0x3c, 0x96, 0x63, 0x88, 0x92,
+	0xc6, 0x9e, 0xc5, 0x4a, 0x2a, 0x0b, 0x52, 0x8b, 0x93, 0xd8, 0xc0, 0xa5, 0x84, 0x31, 0x20, 0x00,
+	0x00, 0xff, 0xff, 0x68, 0x0b, 0x48, 0xe2, 0xd9, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -393,6 +485,7 @@ type MsgClient interface {
 	UpdateParams(ctx context.Context, in *MsgUpdateParams, opts ...grpc.CallOption) (*MsgUpdateParamsResponse, error)
 	CreatePost(ctx context.Context, in *MsgCreatePost, opts ...grpc.CallOption) (*MsgCreatePostResponse, error)
 	UpdatePost(ctx context.Context, in *MsgUpdatePost, opts ...grpc.CallOption) (*MsgUpdatePostResponse, error)
+	DeletePost(ctx context.Context, in *MsgDeletePost, opts ...grpc.CallOption) (*MsgDeletePostResponse, error)
 }
 
 type msgClient struct {
@@ -430,6 +523,15 @@ func (c *msgClient) UpdatePost(ctx context.Context, in *MsgUpdatePost, opts ...g
 	return out, nil
 }
 
+func (c *msgClient) DeletePost(ctx context.Context, in *MsgDeletePost, opts ...grpc.CallOption) (*MsgDeletePostResponse, error) {
+	out := new(MsgDeletePostResponse)
+	err := c.cc.Invoke(ctx, "/testchain.testchain.Msg/DeletePost", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // MsgServer is the server API for Msg service.
 type MsgServer interface {
 	// UpdateParams defines a (governance) operation for updating the module
@@ -437,6 +539,7 @@ type MsgServer interface {
 	UpdateParams(context.Context, *MsgUpdateParams) (*MsgUpdateParamsResponse, error)
 	CreatePost(context.Context, *MsgCreatePost) (*MsgCreatePostResponse, error)
 	UpdatePost(context.Context, *MsgUpdatePost) (*MsgUpdatePostResponse, error)
+	DeletePost(context.Context, *MsgDeletePost) (*MsgDeletePostResponse, error)
 }
 
 // UnimplementedMsgServer can be embedded to have forward compatible implementations.
@@ -451,6 +554,9 @@ func (*UnimplementedMsgServer) CreatePost(ctx context.Context, req *MsgCreatePos
 }
 func (*UnimplementedMsgServer) UpdatePost(ctx context.Context, req *MsgUpdatePost) (*MsgUpdatePostResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdatePost not implemented")
+}
+func (*UnimplementedMsgServer) DeletePost(ctx context.Context, req *MsgDeletePost) (*MsgDeletePostResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeletePost not implemented")
 }
 
 func RegisterMsgServer(s grpc1.Server, srv MsgServer) {
@@ -511,6 +617,24 @@ func _Msg_UpdatePost_Handler(srv interface{}, ctx context.Context, dec func(inte
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Msg_DeletePost_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MsgDeletePost)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MsgServer).DeletePost(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/testchain.testchain.Msg/DeletePost",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MsgServer).DeletePost(ctx, req.(*MsgDeletePost))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "testchain.testchain.Msg",
 	HandlerType: (*MsgServer)(nil),
@@ -526,6 +650,10 @@ var _Msg_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "UpdatePost",
 			Handler:    _Msg_UpdatePost_Handler,
+		},
+		{
+			MethodName: "DeletePost",
+			Handler:    _Msg_DeletePost_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -739,6 +867,64 @@ func (m *MsgUpdatePostResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *MsgDeletePost) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgDeletePost) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgDeletePost) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Id != 0 {
+		i = encodeVarintTx(dAtA, i, uint64(m.Id))
+		i--
+		dAtA[i] = 0x10
+	}
+	if len(m.Creator) > 0 {
+		i -= len(m.Creator)
+		copy(dAtA[i:], m.Creator)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.Creator)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *MsgDeletePostResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgDeletePostResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgDeletePostResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintTx(dAtA []byte, offset int, v uint64) int {
 	offset -= sovTx(v)
 	base := offset
@@ -832,6 +1018,31 @@ func (m *MsgUpdatePost) Size() (n int) {
 }
 
 func (m *MsgUpdatePostResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgDeletePost) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Creator)
+	if l > 0 {
+		n += 1 + l + sovTx(uint64(l))
+	}
+	if m.Id != 0 {
+		n += 1 + sovTx(uint64(m.Id))
+	}
+	return n
+}
+
+func (m *MsgDeletePostResponse) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -1418,6 +1629,157 @@ func (m *MsgUpdatePostResponse) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: MsgUpdatePostResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgDeletePost) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgDeletePost: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgDeletePost: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Creator", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Creator = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
+			}
+			m.Id = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Id |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgDeletePostResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgDeletePostResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgDeletePostResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
